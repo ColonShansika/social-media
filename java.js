1105a190961d253e7fb94e9c5e4dae48adee5a05
ColonShansika <shaniska84@gmail.com>
2025-04-14 13:13:42 -0400

    Update java.js

--- java.js
+++ java.js
@@ -1,12 +1,12 @@
-const Social MediaDiv = document.getElementById('Social Media');
-const Alice CoachmanDiv = document.getElementById('Alice Coachman');
+const SocialMediaDiv = document.getElementById('Social Media');
+const AliceCoachmanDiv = document.getElementById('Alice Coachman');
 const infoDiv = document.getElementById('info');
 
-Alice CoachmanDiv.addEventListener('click', function() {
+AliceCoachmanDiv.addEventListener('click', function() {
   infoDiv.innerHTML = 'This is Alice Coachman info';
 });
 
-Social MediaDiv.addEventListener('click', function() {
+SocialMediaDiv.addEventListener('click', function() {
   infoDiv.innerHTML = 'This is Social Media info';
 });
 
