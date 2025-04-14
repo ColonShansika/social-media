fd669a6a46e62db700e907047913323bc2cdfc06
ColonShansika <shaniska84@gmail.com>
2025-04-14 13:00:06 -0400

    Update java.js

--- java.js
+++ java.js
@@ -52,7 +52,7 @@ for (let i = 1; i <= 20; i++) {
   const p = document.createElement("p");
   p.textContent = i;
 
-  if (i _____) {
+  if (i % 2== 0) {
     p.style.color = "blue";
   } else {
     p.style.color = "red";
